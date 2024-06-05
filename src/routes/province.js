c2d306799b8a0d5a3e2358847a6ced9870178339
TrangLe <letrangle2002@gmail.com>
2024-06-06 02:20:52 +0700

    fix api province VietNam

--- src/routes/province.js
+++ src/routes/province.js
@@ -1,8 +1,20 @@
 import express from 'express'
 import * as controllers from '../controllers/province'
-// CRUD
+import db from '../../data/db.json';
 const router = express.Router()
 
 router.get('/all', controllers.getProvinces)
-
+router.get('/provinces', (req, res) => {
+    res.json(db.province);
+});
+router.get('/districts/:provinceId', (req, res) => {
+    const { provinceId } = req.params;
+    const districts = db.district.filter(d => d.idProvince === provinceId);
+    res.json(districts);
+});
+router.get('/communes/:districtId', (req, res) => {
+    const { districtId } = req.params;
+    const communes = db.commune.filter(c => c.idDistrict === districtId);
+    res.json(communes);
+});
 export default router
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,11 +3,11 @@ import verifyToken from '../middlewares/verifyToken'
 import * as userController from '../controllers/user'
 
 const router = express.Router()
-
+router.get('/count-allUser', userController.countUser)
+router.get('/count-by-moth', userController.countUsersFromMay2024);
 router.use(verifyToken)
 router.get('/get-current', userController.getCurrent)
 router.get('/get-all-user', userController.getAllUser)
 router.put('/update-profile', userController.updateProfile)
-router.get('/count-allUser', userController.countUser)
-router.get('/count-by-moth', userController.countUsersFromMay2024);
+
 export default router
